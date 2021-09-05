59003ad6937b2a8313ce978bd0ada3f4e90f5ce5
coco <2856289571@qq.com>
2021-09-05 21:15:32 +0800

    第一次

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 浏览器不显示非生产环境打包的提示
 Vue.config.productionTip = false
+
+/* eslint-disable no-unused-vars */
+// const a = 123
 
 new Vue({
   render: h => h(App),
